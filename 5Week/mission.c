3edcce56f6b563c1c3d4fbc74e1e5452de5e2904
HyeonJin127 <guswls041207@naver.com>
2025-04-04 23:47:35 +0900

    Add files via upload

---
+++ 5Week/mission.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // 사용자로부터 입력 받기
+    printf("0~255 사이의 정수를 입력하세요: ");
+    scanf("%d", &num);
+
+    // 입력값 범위 확인
+    if (num < 0 || num > 255) {
+        printf("입력값이 범위를 벗어났습니다.\n");
+        return 1;
+    }
+
+    // 8비트 이진수 출력 및 1의 개수 세기
+    int count = 0;
+    printf("8비트 2진수: ");
+    for (int i = 7; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+        if (bit == 1) {
+            count++;
+        }
+    }
+    printf("\n");
+
+    // 1의 개수 출력
+    printf("1의 개수: %d개\n", count);
+
+    // 상위 4비트 출력
+    int upper4 = (num >> 4) & 0x0F;  // 상위 4비트 추출
+    printf("상위 4비트: ");
+    for (int i = 3; i >= 0; i--) {
+        printf("%d", (upper4 >> i) & 1);
+    }
+    printf("\n");
+
+    return 0;
+}
